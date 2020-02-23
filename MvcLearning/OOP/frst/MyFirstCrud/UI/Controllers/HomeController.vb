3aea76f3c657ddaae18d33c6c6b70dd3479f3acd
Uri <Redler.uri@gmail.com>
2020-02-23 19:57:58 +0200

    bn

---
+++ MvcLearning/OOP/frst/MyFirstCrud/UI/Controllers/HomeController.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class HomeController
+        Inherits Controller
+
+        ' GET: Home
+        Function bootstrap() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
