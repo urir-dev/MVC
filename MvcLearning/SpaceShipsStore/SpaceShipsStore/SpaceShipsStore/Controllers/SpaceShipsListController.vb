df80c86fb735e3e56a771b6136607d0873d4aa33
Uri <Redler.uri@gmail.com>
2020-02-26 12:00:41 +0200

    j

--- MvcLearning/SpaceShipsStore/SpaceShipsStore/SpaceShipsStore/Controllers/SpaceShipsListController.vb
+++ MvcLearning/SpaceShipsStore/SpaceShipsStore/SpaceShipsStore/Controllers/SpaceShipsListController.vb
@@ -1,12 +1,41 @@
 ﻿Imports System.Web.Mvc
 
 Namespace Controllers
-    Public Class SpaceShipsListController
+    Public Class ProductController
         Inherits Controller
 
-        ' GET: SpaceShipsList
+        ' GET: Product
         Function Index() As ActionResult
-            Return View()
+            Dim manager = New SpaceShipsManager()
+
+            Dim products = manager.GetAll()
+
+            Return View(products)
+        End Function
+        <HttpGet>
+        Function Edit(id As String) As ActionResult
+            Dim manager = New SpaceShipsManager()
+            Dim product = manager.GetProduct(id)
+
+            Return View(product)
+        End Function
+        <HttpPost>
+        Function Edit(collection As FormCollection) As ActionResult
+            Dim manager = New SpaceShipsManager()
+            Dim id = collection("CatalogID")
+            Dim Ship = manager.GetShip(id)
+            Ship.Name = collection("Name")
+            manager.UpdateProduct(Ship)
+            Return RedirectToAction("Index")
+        End Function
+
+
+        Function List() As ActionResult
+            Dim manager = New SpaceShipsManager
+            Dim Ships = manager.GetAll()
+            Return View(Ships)
+
+
         End Function
     End Class
 End Namespace
