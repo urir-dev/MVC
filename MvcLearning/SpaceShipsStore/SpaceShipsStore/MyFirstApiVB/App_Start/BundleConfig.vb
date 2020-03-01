bf5f8fc7667c4da887661f0e2dc164b8ea3c102a
Uri <Redler.uri@gmail.com>
2020-03-01 18:03:55 +0200

    hnnn
    hhh

--- MvcLearning/SpaceShipsStore/SpaceShipsStore/MyFirstApiVB/App_Start/BundleConfig.vb
+++ MvcLearning/SpaceShipsStore/SpaceShipsStore/MyFirstApiVB/App_Start/BundleConfig.vb
@@ -4,6 +4,7 @@ Imports System.Web.Optimization
 Public Module BundleConfig
     ' For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
     Public Sub RegisterBundles(ByVal bundles As BundleCollection)
+        bundles.Add(New ScriptBundle("~/bundles/jquery").Include(
                    "~/Scripts/jquery-{version}.js"))
 
         ' Use the development version of Modernizr to develop with and learn from. Then, when you're
